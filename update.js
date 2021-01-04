00706ddd8f33c1da14f1295fa9a59751164f54d9
cospplredman <62368514+cospplredman@users.noreply.github.com>
2021-01-04 17:53:40 -0600

    Update update.js

--- update.js
+++ update.js
@@ -4,13 +4,13 @@ let N = 0;
 class quadTree{
 	this.b = {};
 	function eq(f){
-		else if
-			(
-				(f.b[0] == this.b[0] || f.b[0].eq(f.b[0])) &&
-				(f.b[1] == this.b[1] || f.b[1].eq(f.b[1])) &&
-				(f.b[2] == this.b[2] || f.b[2].eq(f.b[2])) &&
-				(f.b[3] == this.b[3] || f.b[3].eq(f.b[3]))
-			)
+		if
+		(
+			(f.b[0] == this.b[0] || f.b[0].eq(f.b[0])) &&
+			(f.b[1] == this.b[1] || f.b[1].eq(f.b[1])) &&
+			(f.b[2] == this.b[2] || f.b[2].eq(f.b[2])) &&
+			(f.b[3] == this.b[3] || f.b[3].eq(f.b[3]))
+		)
 			return true;
 	}
 }
