542c25c9834a7da0be2cb7e11a62209aa8d35777
cospplredman <62368514+cospplredman@users.noreply.github.com>
2021-01-04 18:43:25 -0600

    Update update.js

--- update.js
+++ update.js
@@ -1,14 +1,23 @@
 class quadTree{
 	this.b = {};
 	function eq(f){
-		if
-		(
-			(f.b[0] == this.b[0] || f.b[0].eq(f.b[0])) &&
-			(f.b[1] == this.b[1] || f.b[1].eq(f.b[1])) &&
-			(f.b[2] == this.b[2] || f.b[2].eq(f.b[2])) &&
-			(f.b[3] == this.b[3] || f.b[3].eq(f.b[3]))
-		)
-			return true;
+		for(let i = 0; i != 4; i++)
+			if(!(f.b[i] == this.b[1] || f.b[1].eq(f.b[1])))
+				return false;
+		return true;
+	}
+	function ascend(){
+		for(let i = 0; i != 4; i++)
+			if(this.b[i]) let k = new quadTree(), this.b[i].b[(i + 2) % 3] = this.b[i], this.b[i] = k;
+	}
+	function set(x,y,v){
+		let f = x|y;
+		if(f > N)
+			while(N < f){
+				N <<= 1;
+				this.ascend();
+			}
+		//todo finish set
 	}
 }
 
